bd09d183d474ab587f5d609dc5be843694305afc
Myra Smith <61550209+myrasmith@users.noreply.github.com>
2021-05-11 08:56:57 -0400

    Update Results.js

--- src/Components/Results.js
+++ src/Components/Results.js
@@ -5,7 +5,9 @@ class Result extends Component {
 
     render() {
         let {result} = this.props;
-        
+        // Here is where you should insert commas into result.
+        // Because result could be a string like "65 + 7" it's a bit harder to insert commas in the correct spots.
+        // But definitely not impossible!! Try writing a helper function that accepts result as an arg and returns a properly formatted string with commas.
         return (
             <div className="result">
                 <p>{result || 0}</p>
